9865b90e19eeb890c01805927d94d64574d458bd
Raphaël Tétreault <41877613+RaphaelTetreault@users.noreply.github.com>
2024-06-05 14:03:17 -0400

    Add VS project

---
+++ awesome-team-a4-game/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace awesome_team_a4_game
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
